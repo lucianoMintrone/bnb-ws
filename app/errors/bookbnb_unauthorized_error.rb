dea117152139ddf54ad7ac1e7cbd593647b4d64c
Luciano Amalgama <luciano@amalgama.co>
2020-11-04 23:35:38 -0300

    Close #3.3 - Return 401 when token has expired

--- app/errors/bookbnb_unauthorized_error.rb
+++ app/errors/bookbnb_unauthorized_error.rb
@@ -1,7 +1,5 @@
 class BookbnbUnauthorizedError < BookbnbError
-	def initialize
-		error = 'unauthorized'
-		error_message = "Authorization Required"
+	def initialize(error: 'unauthorized', error_message: "Authorization Required")
 		super error, error_message, 401
 	end
 end
